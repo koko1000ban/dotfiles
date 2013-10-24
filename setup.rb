51edfc781ed790e8f037433377445f571bb4c28c
tabito ohtani <koko1000ban@gmail.com>
2013-10-24 17:32:01 +0900

    1. Added submodules 2. Modified setup.rb

--- setup.rb
+++ setup.rb
@@ -4,7 +4,7 @@ DEBUG = ARGV.first == "--dry-run"
 
 d = Dir
     .glob('*',File::FNM_DOTMATCH)
-    .reject{|n| n =~ %r!^((\.svn)|(\.{1,2}|(\.git)|(\.DS_Store)))$! }
+    .reject{|n| n =~ %r!^((\.svn)|(\.{1,2}|(\.git)|(\.gitmodules)|(\.DS_Store)))$! }
     .reject{|n| n =~ %r!^setup\.(sh|rb)!}
 
 d.each do |f|
